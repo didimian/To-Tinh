f33ceb34096ecb662cef1c859ef12b03e7275a13
didimian <89386885+didimian@users.noreply.github.com>
2021-08-24 02:37:45 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,16 +1,16 @@
 const CONFIG = {
-    introTitle: 'Xin chào bạn iu!',
+    introTitle: 'Xin chào bạn iu❣️',
     introDesc: `Sẵn sàng bước sang tuổi 20 chưa? 🥳`,
     btnIntro: 'Rùiii‼️',
     title: 'Chúc anh sinh nhật tuổi 20 tuyệt zời 🎂❤️',
     desc: 'Tuổi mới luôn vui vẻ, hạnh phúc, mạnh khoẻ, thành công và yêu em hơn 10000000 lần nữa 🥰',
     btnYes: 'Vâng ạ 😍',
     btnNo: 'Khum 🥺',
-    question:'Nhập mật khẩu của chúng mình để mở khoá 🔐',
+    question:'Nhập mật khẩu của chúng mình để 🔓',
     btnReply: 'Gửi đi 📩',
     reply: '1706',
-    mess: 'Tuổi 20 và sau này mãi vui vẻ và hạnh phúc nha 🙆🏻‍♀️',
-    messDesc: 'Yêu anh 💖',
+    mess: 'Tuổi 20 và sau này mãi vui vẻ và hạnh phúc nha 🙆🏻‍♀️💝💗💓',
+    messDesc: 'Yêu anh 💖❤️🧡💛💚💙💜🤎💝',
     btnAccept: '🙆🏽‍♂️🙆🏽‍♂️',
     messLink: 'https://drive.google.com/file/d/14K4Yn1Esi-wgloAdo0mB9S27BQTc9uiA/view?usp=drivesdk' 
 }
